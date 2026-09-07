a2dfe0af814a91dfac49e038e056e110894aeb6c
yovanynahuat-admin <cyovanyemmanuel@gmail.com>
2026-09-07 17:44:21 -0600

    Aprendi a implementar depedency injection

---
+++ OmniReserve.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OmniReserve.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Aquí se registrarán posteriormente los servicios de Infrastructure.
+
+        return services;
+    }
+}
