63047f45e32da026cd73b18f8b626a02fa9de6fc
yovanynahuat-admin <cyovanyemmanuel@gmail.com>
2026-09-13 19:28:14 -0600

    Aprendi a encapsular inyección de dependencias por capas

--- OmniReserve.Api/Program.cs
+++ OmniReserve.Api/Program.cs
@@ -1,9 +1,15 @@
+using OmniReserve.Application;
+using OmniReserve.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -11,6 +17,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
+
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
@@ -20,4 +27,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
