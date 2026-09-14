7df6f2354079f8ffe1813c1e9a8be9b045b8665a
yovanynahuat-admin <cyovanyemmanuel@gmail.com>
2026-09-13 20:40:28 -0600

    Lo que hice con Queries y aprendí a implementar CQRS para consultar habitaciones

---
+++ OmniReserve.Application/Rooms/Queries/GetRoomById/GetRoomByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace OmniReserve.Application.Rooms.Queries.GetRoomById;
+
+public class GetRoomByIdQueryHandler : IRequestHandler<GetRoomByIdQuery, RoomResponseDto>
+{
+    public Task<RoomResponseDto> Handle(
+        GetRoomByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var room = new RoomResponseDto
+        {
+            Id = request.RoomId,
+            RoomNumber = "101",
+            Type = "Double",
+            Price = 1500,
+            IsAvailable = true
+        };
+
+        return Task.FromResult(room);
+    }
+}
---
+++ OmniReserve.Application/Rooms/Queries/GetRoomById/RoomResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OmniReserve.Application.Rooms.Queries.GetRoomById;
+
+public class RoomResponseDto
+{
+    public Guid Id { get; set; }
+    public string RoomNumber { get; set; }
+    public string Type { get; set; }
+    public decimal Price { get; set; }
+    public bool IsAvailable { get; set; }
+}
